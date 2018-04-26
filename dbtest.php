05c20b2564e2bc86df48d30cac59ec6681a9476e
Srikant <srikantdaphins@gmail.com>
2018-04-26 11:36:39 +0530

    MayBe

--- dbtest.php
+++ dbtest.php
@@ -1,6 +1,8 @@
 <?php
-$connection = new mysqli('127.0.0.1:3306', 'sri', '1234', 'attendancemgmtsystem');
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$connection = new mysqli($dbhost, 'root', '1234', 'attendancemgmtsystem');
 if ($connection->connect_errno) {
+	printf("Connect failed:, $dbhost");
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
