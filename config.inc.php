871eff9c0061e8d57ead8e848511533a61204bcd
Srikant <srikantdaphins@gmail.com>
2018-04-26 11:00:29 +0530

    MayBe

--- config.inc.php
+++ config.inc.php
@@ -29,6 +29,7 @@ $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
+$cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
 
