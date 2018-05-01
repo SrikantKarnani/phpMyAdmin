45a42f9f98038e0cddd7025ff0f477f0ec59b69b
Srikant Karnani <srikant.karnani352@gmail.com>
2018-05-01 13:34:38 +0530

    Y

--- config.inc.php
+++ config.inc.php
@@ -9,7 +9,6 @@
  *
  * @package PhpMyAdmin
  */
-
 /**
  * This is needed for cookie based authentication to encrypt password in
  * cookie. Needs to be 32 chars long.
@@ -28,7 +27,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = $HOSTNAME';
+$cfg['Servers'][$i]['host'] = $HOSTNAME;
 $cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
