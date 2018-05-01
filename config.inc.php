442c0416645a9c8a4484d072efa126f78747a92e
Srikant Karnani <srikant.karnani352@gmail.com>
2018-05-01 13:46:46 +0530

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -27,10 +27,10 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = $HOSTNAME;
+$cfg['Servers'][$i]['host'] = 'mysql';
 $cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
-$cfg['Servers'][$i]['AllowNoPassword'] = true;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 /**
  * phpMyAdmin configuration storage settings.
